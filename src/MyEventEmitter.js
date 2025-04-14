575ea57b89a767a738ee37537a5103e6757727d4
yarkob <yaroslav.klark.bondarenko@gmail.com>
2025-04-14 17:23:43 +0300

    initial commit

---
+++ src/MyEventEmitter.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class MyEventEmitter {
+  on() {}
+  once() {}
+  off() {}
+  emit() {}
+  prependListener() {}
+  prependOnceListener() {}
+  removeAllListeners() {}
+  listenerCount() {}
+}
+
+module.exports = MyEventEmitter;
